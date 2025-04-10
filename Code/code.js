a285b4027608dac18fb02f871e95c02e4cb3366d
tauonlinecourses <47775661+tauonlinecourses@users.noreply.github.com>
2025-04-10 13:58:33 +0300

    Run

---
+++ Code/code.js
@@ -0,0 +1,64 @@
+const the_animation = document.getElementsByClassName('have-scroll-animation');
+
+document.addEventListener("DOMContentLoaded", function() {
+    loudAllImages();
+    start();
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F1') {
+        event.preventDefault(); // Prevent the default action (e.g., opening help in some browsers)
+        start();
+    }
+});
+
+function start() {
+    for (i = 0; i < the_animation.length; i++) {
+        el = the_animation[i];
+        observer.observe(el)
+    }
+}
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active-enter-animation');
+        }
+        else if (chackAnimationCondition(entry.target, false)) {
+            entry.target.classList.remove('active-enter-animation');
+        }
+    })
+});
+
+function chackAnimationCondition(object, isAddingAnimation) {
+    canChangeAnimation = true;
+    if (isAddingAnimation) {
+
+    }
+    else {
+        canChangeAnimation = !object.classList.contains("loop-Once-animation");
+    }
+    return canChangeAnimation;
+
+}
+
+function loudAllImages(){
+    const imagePromises = Array.from(document.images).map(img => {
+        if (!img.complete) {
+            return new Promise(resolve => {
+                img.addEventListener('load', resolve);
+                img.addEventListener('error', resolve);
+            });
+        } else {
+            return Promise.resolve();
+        }
+    });
+    
+    // Wait for all image Promises to resolve
+    Promise.all(imagePromises).then(() => {
+        console.log("stopLoading");
+        document.getElementById("loadingScreen").remove();
+        // Your additional code here
+    });
+}
